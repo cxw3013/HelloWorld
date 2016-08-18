a3df25d5257073466a332ee200fbd327088841a4
cxw <cxw3013@163.com>
2016-08-18 20:35:14 +0800

    ActionSheetIOS
    ActionSheetIOS test

--- UIStudy/index.ios.js
+++ UIStudy/index.ios.js
@@ -4,49 +4,72 @@
  * @flow
  */
 
+"use strict";
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  UIManager,
+  ActionSheetIOS,
 } from 'react-native';
 
-class UIStudy extends Component {
+let BUTTONS;
+BUTTONS = [
+    'Option 0',
+    'Option 1',
+    'Option 2',
+    'Delete',
+    'Cancel',
+];
+ const DESTRUCTIVE_INDEX = 3;
+ const CANCEL_INDEX = 4;
+ class UIStudy extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            clicked:'none',
+        };
+        //bind(this)很重要
+        this.showActionSheet = this.showActionSheet.bind(this);
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View style={{marginTop:200}}>
+        <Text onPress = {this.showActionSheet} style = {styles.button}>
+            Click to show ActionSheet
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+            Clicked button:{this.state.clicked}
         </Text>
       </View>
     );
-  }
+}
+//ActionSheet
+showActionSheet() {
+    ActionSheetIOS.showActionSheetWithOptions({
+        options:BUTTONS,
+        cancelButtonIndex:CANCEL_INDEX,
+        destructiveButtonIndex:DESTRUCTIVE_INDEX,
+        title:'This is a Title',//显示的标题
+        message:'This is the message',
+        tintColor:'blue',//控制颜色
+    },
+    (buttonIndex) => {
+        let selectedTitle = BUTTONS[buttonIndex];
+        console.log(buttonIndex + selectedTitle);
+        this.setState({clicked:BUTTONS[buttonIndex]});
+    }
+);
+}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  button: {
+      marginBottom:10,
+      fontWeight:'500',
   },
 });
 
